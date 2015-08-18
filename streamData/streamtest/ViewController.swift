f94f474b52966504dc1a9ea3ec89f90d24460791
DrkSephy <dleonar00@citymail.cuny.edu>
2015-08-18 13:31:01 -0700

    Adding content for non-ad stream cells, tweaking layout

--- streamData/streamtest/ViewController.swift
+++ streamData/streamtest/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController, FlurryAdNativeDelegate, UITableViewDeleg
     @IBOutlet var tableView: UITableView!
 
     var tableData: [AnyObject] = [];
-    var otherData = ["David", "Leonard"];
     let nativeAd = FlurryAdNative(space: "streamy");
     
     override func viewDidLoad() {
@@ -29,19 +28,25 @@ class ViewController: UIViewController, FlurryAdNativeDelegate, UITableViewDeleg
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.tableData.count + self.otherData.count;
+        return 3;
     }
-    
+
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let cell = self.tableView.dequeueReusableCellWithIdentifier("cell") as! TblCell;
-        println(indexPath.row);
         
-        if(indexPath.row % 2 == 0) {
-            cell.streamTitleLabel.text = "even";
+        
+        if(indexPath.row == 0) {
+            cell.streamTitleLabel.text = "Creature -- Angel";
+            cell.streamDescriptionLabel.font = UIFont.italicSystemFontOfSize(16);
+            cell.streamDescriptionLabel.text = "Even the most battle-hardened soldiers pause in her presence for a moment of introspection.";
+            cell.sponsoredImageView.image = nil;
+            cell.streamImageView.image = UIImage(named: "card1")!;
+            cell.streamSourceLabel.text = "Stoic Angel";
+            cell.sponsoredText.text = "";
         }
         
-        if(indexPath.row % 2 == 1) {
+        if(indexPath.row == 1) {
             if(self.tableData.count > 0) {
                 for var index = 0; index < self.tableData[0].count; index++ {
                     var newAsset: AnyObject = self.tableData[0][index];
@@ -77,20 +82,31 @@ class ViewController: UIViewController, FlurryAdNativeDelegate, UITableViewDeleg
                     if (newAsset.name == "source") {
                         cell.streamSourceLabel.text = newAsset.value;
                     }
+                    
+                    cell.sponsoredText.text = "sponsored";
                 }
             }
         }
-        return cell;
         
+        if(indexPath.row == 2) {
+            cell.streamTitleLabel.text = "Artifact";
+            cell.streamDescriptionLabel.font = UIFont.italicSystemFontOfSize(16);
+            cell.streamDescriptionLabel.text = "Adds 3 mana of any single color of your choice to your mana pool, then is discarded. Tapping this artifact can be played as an interrupt.";
+            cell.sponsoredImageView.image = nil;
+            cell.streamImageView.image = UIImage(named: "card2")!;
+            cell.streamSourceLabel.text = "Black Lotus";
+            cell.sponsoredText.text = "";
+        }
+        
+        return cell;
     }
 
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 200;
+        return 400;
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func adNativeDidFetchAd(nativeAd: FlurryAdNative!) {
@@ -98,6 +114,5 @@ class ViewController: UIViewController, FlurryAdNativeDelegate, UITableViewDeleg
         self.tableData.append(asset);
         self.tableView.reloadData();
     }
-    
 }
 
