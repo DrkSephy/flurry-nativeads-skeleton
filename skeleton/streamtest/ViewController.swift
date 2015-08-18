92bdcd10c5be706ed071f42fe8c185ec33f50b6c
DrkSephy <dleonar00@citymail.cuny.edu>
2015-08-18 13:52:26 -0700

    Adding default content to skeleton application

--- skeleton/streamtest/ViewController.swift
+++ skeleton/streamtest/ViewController.swift
@@ -23,19 +23,38 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.tableData.count;
+        return 2;
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         var cell:TblCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as! TblCell;
+        if(indexPath.row == 0) {
+            cell.streamTitleLabel.text = "Creature -- Angel";
+            cell.streamDescriptionLabel.font = UIFont.italicSystemFontOfSize(16);
+            cell.streamDescriptionLabel.text = "Even the most battle-hardened soldiers pause in her presence for a moment of introspection.";
+            cell.sponsoredImageView.image = nil;
+            cell.streamImageView.image = UIImage(named: "card1")!;
+            cell.streamSourceLabel.text = "Stoic Angel";
+            cell.sponsoredText.text = "";
+        }
+        
+        if(indexPath.row == 1) {
+            cell.streamTitleLabel.text = "Artifact";
+            cell.streamDescriptionLabel.font = UIFont.italicSystemFontOfSize(16);
+            cell.streamDescriptionLabel.text = "Adds 3 mana of any single color of your choice to your mana pool, then is discarded. Tapping this artifact can be played as an interrupt.";
+            cell.sponsoredImageView.image = nil;
+            cell.streamImageView.image = UIImage(named: "card2")!;
+            cell.streamSourceLabel.text = "Black Lotus";
+            cell.sponsoredText.text = "";
+        }
         
         return cell;
         
     }
 
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 200;
+        return 400;
     }
     
     override func didReceiveMemoryWarning() {
